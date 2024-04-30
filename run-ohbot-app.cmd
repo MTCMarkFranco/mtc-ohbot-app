167ddd56c2bb18f0d8bfcb2f6663489da1107e4c
MTCMarkFranco <mark.franco@microsoft.com>
2024-04-30 13:19:39 -0400

    Upped the delay between server and app start. to allow ohbot to connect.

--- run-ohbot-app.cmd
+++ run-ohbot-app.cmd
@@ -6,7 +6,7 @@ rem timeout 10
 @echo Starting Service...
 start /MIN %USERPROFILE%\AppData\Local\Programs\Python\Python36\python.exe .\server\ohbot_service.py
 
-timeout 10
+timeout 20
 
 @echo Starting App...
 %USERPROFILE%\AppData\Local\Programs\Python\Python311\python.exe .\app\ohbot_openai_app.py
